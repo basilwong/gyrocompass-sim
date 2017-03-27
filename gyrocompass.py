edb87b66a6b0d992de5873d78ee31a8913acc686
Walker Shanks <walkershanks@hotmail.com>
2017-03-26 19:54:07 -0700

    python file with a support and a ring

---
+++ gyrocompass.py
@@ -0,0 +1,9 @@
+from visual import *
+
+
+## create the suppport for the outer ring
+base = pyramid(pos = (0,-5,0), axis = (0,1,0), size = (1.5, 1.5, 1.5))
+
+## this is the outer ring, might be part of a frame later?
+ring(pos = (0,0.5,0), axis = (0,0,1), radius = 4, thickness = 0.1, color = (0.4,0.1,0.3))
+
