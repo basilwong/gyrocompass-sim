57ee114b207ff7ef088a7ca2013f5843dd857bd9
Walker Shanks <walkershanks@hotmail.com>
2017-03-27 17:02:23 -0700

    no more hardcoding, everything is now in a variable

--- basilgyro.py
+++ basilgyro.py
@@ -1,17 +1,37 @@
 from visual import *
 
-## create the suppport for the outer ring 
-base = pyramid(pos = (0,-12.1 - 1.5,0), axis = (0,1,0), size = (1.5, 1.5, 1.5))
+##constants
+BASE_POSITION = (0,-12.1 - 1.5,0)
+BASE_AXIS = (0,1,0)
+BASE_SIZE = (1.5, 1.5, 1.5)
 
-#Inner Rotating Circle
-rod = cylinder(pos=(0,0,0), axis=(0.3,0,0), radius=10)
+ROD_POSITION = (0,0,0)
+ROD_AXIS = (0.3,0,0)
+ROD_RADIUS = 10
 
-#Circle Supports
-rod = cylinder(pos=(-11,0,0), axis=(22,0,0), radius=0.3)
+GYRO_POSITION = (-11,0,0)
+GYRO_AXIS = (22,0,0)
+GYRO_RADIUS = 0.3
 
-#XZ Plane Ring
-ring(pos = (0,0,0), axis = (0,1,0), radius = 11.1, thickness = 0.1, color = (0.9,0.5,6))
+IRING_POSITION = (0,0,0)
+IRING_AXIS = (0,1,0)
+IRING_RADIUS = 11.1
+IRING_THICKNESS = 0.1
+IRING_COLOUR = (0.9,0.5,6)
 
-## this is the outer ring, might be part of a frame later? (Y- Plane ring)
-ring(pos = (0,0,0), axis = (0,0,1), radius = 12, thickness = 0.1, color = (0.4,0.1,0.3))
+ORING_POSITION = (0,0,0)
+ORING_AXIS = (0,0,1)
+ORING_RADIUS = 12
+ORING_THICKNESS = 0.1
+ORING_COLOUR =(0.4,0.1,0.3)
+
+f1 = frame()    ## frame containing rod and gyro
+
+
+
+base = pyramid(pos = BASE_POSITION, axis = BASE_AXIS, size = BASE_SIZE) ## create the suppport for the outer ring 
+rod = cylinder(frame = f1, pos= ROD_POSITION, axis= ROD_AXIS , radius= ROD_RADIUS) #Inner Rotating Circle
+gyro = cylinder(frame = f1, pos= GYRO_POSITION, axis= GYRO_AXIS, radius= GYRO_RADIUS) #Circle Supports
+IRING = ring(pos = IRING_POSITION , axis = IRING_AXIS, radius = IRING_RADIUS, thickness = IRING_THICKNESS, color = IRING_COLOUR) #XZ Plane Ring
+ORING = ring(pos = ORING_POSITION, axis = ORING_AXIS, radius = ORING_RADIUS, thickness = ORING_THICKNESS, color = ORING_COLOUR ) ## this is the outer ring, might be part of a frame later? (Y- Plane ring)
 
--- gyrocompass.py
+++ gyrocompass.py
@@ -1,9 +1,18 @@
 from visual import *
 
 
+
+## frame that comprises the rod and the gyro
+f1 = frame()
+#Inner Rotating Circle
+rod = cylinder(frame = f1, pos=(0,0,0), axis=(1,0,0), radius=10)
+#Circle Supports
+rod = cylinder(frame = f1, pos=(-11,0,0), axis=(22,0,0), radius=0.3)
+
 ## create the suppport for the outer ring
 base = pyramid(pos = (0,-5,0), axis = (0,1,0), size = (1.5, 1.5, 1.5))
 
 ## this is the outer ring, might be part of a frame later?
 ring(pos = (0,0.5,0), axis = (0,0,1), radius = 4, thickness = 0.1, color = (0.4,0.1,0.3))
+
 
