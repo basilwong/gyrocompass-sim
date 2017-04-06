a23c8633f5eca86e74f12ec752655a6e612f2b04
Walker Shanks <walkershanks@hotmail.com>
2017-04-05 23:17:45 -0700

    added dragging, still a tad buggy tho

--- BitchBetterHaveMyMoney.py
+++ BitchBetterHaveMyMoney.py
@@ -2,8 +2,22 @@ from visual import *
 import time
 import numpy as np
 import wx
+import win32gui
+import Image
 import scipy as sp
 from scipy.integrate import odeint
+
+# name = "swirltex"
+# width = 128  # must be power of 2
+# height = 128  # must be power of 2
+# im = Image.open(name + ".jpg")
+
+
+# print(im.size) # optionally, see size of image
+# Optional cropping:
+# im = im.crop((x1,y1,x2,y2)) # (0,0) is upper left
+# im = im.resize((width,height), Image.ANTIALIAS)
+# materials.saveTGA(name,im)
 
 
 # import matplotlib.pyplot as plt
@@ -21,10 +35,10 @@ def solvr(z, t):
     # little_sphere.pos = coordsToSphere(float(np.random.sample()), float(np.random.sample()))
     alpha, alphadot = z
     return [alphadot, (
-    I1 * omega * (phidot - omega * sin(float(lat.GetValue()) * pi / 180 + pi / 2) * cos(alpha)) * sin(
-        (float(lat.GetValue()) * pi / 180 + pi / 2)) * sin(alpha) +
-    1 / 2 * I2 * omega * omega * (sin((float(lat.GetValue()) * pi / 180 + pi / 2))) * (
-    sin((float(lat.GetValue()) * pi / 180) + pi / 2)) * sin(2 * alpha)) / I2 - alphadot * dampeningConstant]
+        I1 * omega * (phidot - omega * sin(float(lat.GetValue()) * pi / 180 + pi / 2) * cos(alpha)) * sin(
+            (float(lat.GetValue()) * pi / 180 + pi / 2)) * sin(alpha) +
+        1 / 2 * I2 * omega * omega * (sin((float(lat.GetValue()) * pi / 180 + pi / 2))) * (
+            sin((float(lat.GetValue()) * pi / 180) + pi / 2)) * sin(2 * alpha)) / I2 - alphadot * dampeningConstant]
 
 
 def coordsToSphere(latitude, longitude):
@@ -58,11 +72,11 @@ def OnEnterPressed(event):
 
 def RunSimulation():
     # Initial conditions on y, y' at x=0
-    init = pi / 2, 0.0
+    init = initial_angle, 0.0
     # First integrate from 0 to 2
-    x = np.linspace(0, 7500, 7500 * 30)
+    x = np.linspace(0, 1500, 1500 * 30)
     # Then integrate from 0 to -2
-    x = np.linspace(0, 7500, 7500 * 30)
+    x = np.linspace(0, 1500, 1500 * 30)
     return odeint(solvr, init, x)
 
 
@@ -93,25 +107,45 @@ def SetDamper(evt):  # called on slider events
     dampeningConstant = value
     OnEnterPressed(evt)
 
+
 def mouseRotate(evt):
-    global mousepos
-    mousepos = evt.pickpos
-    scene1.bind('mousemove',move)
-    scene1.bind('mouseup',stop)
+    global click
+    scene1.bind('mousemove', move)
+    scene1.bind('mouseup', stop)
+    scene2.bind('mousemove',move)
+    scene2.bind('mouseup',stop)
+    click = true
+
 
 def move(evt):
-    global mousepos
-    global dx
-    newpos = scene1.mouse.project(normal=(0,0,1))
-    dx = newpos[0] - mousepos[0]
-    print(dx)
+    global initial_angle
+    global x,y,dx,dtheta
+    mpos = win32gui.GetCursorPos()
+    dx = mpos[0] - x
+    x = mpos[0]
+
+    dtheta = dx*pi/90
+    gyro.axis += (dtheta,0,0)
+    initial_angle = gyro.axis[0] 
+    print dtheta
 
 
 def stop(evt):
-    scene1.unbind('mousemove',move)
+    global click
+    click = false
+    scene1.unbind('mousemove', move)
     scene1.unbind('mouseup', stop)
+    OnEnterPressed(evt)
+
+
 
 ##constants
+dx = 0
+x = 0
+y = 0
+dtheta = 0
+click = false
+
 LEFTMARGIN = 20
 EARTH_RADIUS = 1.0
 BASE_POSITION = (0, -12.1 - 1.5, 0)
@@ -173,12 +207,13 @@ OMEGA_SLIDER_POSY = TEXTBOX1_POSY
 DAMPER_SLIDER_POSX = OMEGA_SLIDER_POSX + LEFTMARGIN + TEXTBOX_WIDTH
 DAMPER_SLIDER_POSY = TEXTBOX1_POSY
 
+initial_angle = 3*pi/4
 Loops_per_second = 30
 should_break = false
 dampeningConstant = 1.5
 sol = np.zeros((9000, 2))
 # omega =5
-omega = 0.00007295
+omega = 0.3
 I1 = .125 * 0.30 * 3 / 2
 I2 = I1 / 2
 delta = pi / 3;
@@ -194,7 +229,7 @@ scene1 = display(window=w, x=d, y=d, width=L - 2 * d, height=L - 2 * d, forward=
 scene2 = display(window=w, x=d + scene1.width, y=d, width=L - 2 * d, height=L - 2 * d, forward=-vector(0, 1, 2))
 
 scene1.bind("mousedown", mouseRotate)
-
+scene2.bind("mousedown", mouseRotate)
 
 ##populate scene1
 scene1.select()
@@ -202,11 +237,12 @@ gyro = frame()
 indicator = arrow(frame=gyro, pos=INDICATOR_POSITION, axis=INDICATOR_AXIS, shaftwidth=INDICATOR_WIDTH,
                   color=INDICATOR_COLOR, opacity=INDICATOR_OPACITY)  # North Indicator
 base = pyramid(pos=BASE_POSITION, axis=BASE_AXIS, size=BASE_SIZE,
-               color=BASE_COLOUR)  ## create the suppport for the outer ring
+               color=BASE_COLOUR, material=materials.emissive)  ## create the suppport for the outer ring
 rod = cylinder(frame=gyro, pos=ROD_POSITION, axis=ROD_AXIS, radius=ROD_RADIUS,
-               color=ROD_COLOUR)  # Inner Rotating Circle
+               color=ROD_COLOUR, material=materials.emissive)  # Inner Rotating Circle
 spinner = cylinder(frame=gyro, pos=GYRO_POSITION, axis=GYRO_AXIS, radius=GYRO_RADIUS,
-                   material=materials.wood)  # Circle Supports
+                   material=materials.wood)
+# dot = points(frame = gyro, pos = spinner.pos + (0.5,1,0.5), color = color.black)
 IRING = ring(pos=IRING_POSITION, axis=IRING_AXIS, radius=IRING_RADIUS, thickness=IRING_THICKNESS,
              color=IRING_COLOUR)  # XZ Plane Ring
 ORING = ring(pos=ORING_POSITION, axis=ORING_AXIS, radius=ORING_RADIUS, thickness=ORING_THICKNESS,
@@ -217,7 +253,7 @@ scene2.select()
 earthframe = frame()
 earth = sphere(frame=earthframe, pos=GYRO_POSITION, material=materials.earth, radius=EARTH_RADIUS)
 little_sphere = arrow(frame=earthframe, pos=earth.pos + (0, 0, earth.radius) + (0, 0, earth.radius / 32),
-                      material=materials.emissive, color=(1, 0, 1), shaftwidth=0.02, length=0.12)
+                      color=(1, 0, 1), shaftwidth=0.02, length=0.12)
 
 p = w.panel  # Refers to the full region of the window in which to place widgets
 
@@ -297,9 +333,13 @@ little_sphere.length = 0.12
 sol = RunSimulation()
 
 while (true):
-    for i in range(0, 7500 * 30):
+
+    for i in range(0, 1500 * 30):
         rate(Loops_per_second)
         wx.Yield()
+        if(click):
+            break
+
         if i % 20 == 0:
             wx.Yield()
             if should_break == true:
